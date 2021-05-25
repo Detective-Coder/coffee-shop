e5fd98a0513123230f44b2e17ac9790f73ce7975
Detective-Coder <aaronfrankenfield@gmail.com>
2021-05-25 13:44:56 -0400

    not sure yet

--- CoffeeShop/Startup.cs
+++ CoffeeShop/Startup.cs
@@ -1,3 +1,4 @@
+using CoffeeShop.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -26,6 +27,7 @@ namespace CoffeeShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddTransient<IBeanVarietyRepository, BeanVarietyRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
